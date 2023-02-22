81b491fb09dc38102113d909e78e3ebff01dd2d0
RNm-dove <m.rynk.plume@gmail.com>
2023-02-22 09:23:37 +0900

    Improve code

--- tsubo/main.js
+++ tsubo/main.js
@@ -1,31 +1,34 @@
 /********************* */
 //    データ置き場        //
 /********************* */
-const tsubo1Info = {
-  id: "tsubo1",
-  name: "親指側",
-  requiredMinimumClickCount: 5,
-} 
-const tsubo2Info = {
-  id: "tsubo2",
-  name: "小指側",
-  requiredMinimumClickCount: 3,
-} 
-const tsubo3Info = {
-  id: "tsubo3",
-  name: "サイド",
-  requiredMinimumClickCount: 1,
-} 
-const tsubo4Info = {
-  id: "tsubo4",
-  name: "つちふまず",
-  requiredMinimumClickCount: 2,
-} 
-const tsubo5Info = {
-  id: "tsubo5",
-  name: "かかと",
-  requiredMinimumClickCount: 1,
-}
+const tsuboList = [
+  {
+    id: "tsubo1",
+    name: "親指側",
+    requiredMinimumClickCount: 5,
+  }, 
+  {
+    id: "tsubo2",
+    name: "小指側",
+    requiredMinimumClickCount: 3,
+  }, 
+  {
+    id: "tsubo3",
+    name: "サイド",
+    requiredMinimumClickCount: 1,
+  }, 
+  {
+    id: "tsubo4",
+    name: "つちふまず",
+    requiredMinimumClickCount: 2,
+  }, 
+  {
+    id: "tsubo5",
+    name: "かかと",
+    requiredMinimumClickCount: 1,
+  }
+];
+
 const accumulateClick = {
   tsubo1: 0,
   tsubo2: 0,
@@ -33,6 +36,8 @@ const accumulateClick = {
   tsubo4: 0,
   tsubo5: 0,
 }
+
+const targetTsuboIds = ['tsubo1', 'tsubo2', 'tsubo5'];
 
 
 /********************* */
@@ -45,124 +50,71 @@ const accumulateClick = {
 /********************* */
 function checkIsDone(){
   // すべてのツボを最低限クリックできていれば　true
-  if(
-    accumulateClick.tsubo1 >= tsubo1Info.requiredMinimumClickCount
-    && accumulateClick.tsubo2 >= tsubo2Info.requiredMinimumClickCount
-    && accumulateClick.tsubo3 >= tsubo3Info.requiredMinimumClickCount
-    && accumulateClick.tsubo4 >= tsubo4Info.requiredMinimumClickCount
-    && accumulateClick.tsubo5 >= tsubo5Info.requiredMinimumClickCount
-  ){
-    return true;
+  let isClear = true;
+
+  for(let i = 0; i < targetTsuboIds.length; i++){
+    const targetTsuboId = targetTsuboIds[i];
+
+    for(let j = 0; j < tsuboList.length; j++){
+      const tsubo = tsuboList[i];
+      if(tsubo.id === targetTsuboId){
+        if(accumulateClick[targetTsuboId] >= tsubo.requiredMinimumClickCount){
+          isClear = isClear && true;
+        } else {
+          // 1つでもfalseが交じると以後false
+          isClear = isClear && false;
+        }
+        break;
+      }
+    }
   }
 
-  return false;
+  return isClear;
 }
+
+function onClickTsubo (tsuboId) {
+
+  accumulateClick[tsuboId] += 1;
+
+  console.log(`${tsuboId} clicked`, accumulateClick[tsuboId])
+
+  if(targetTsuboIds.includes(tsuboId)){
+    // 痛っと表示する。
+    const itaiDiv = document.getElementById("itai");
+    itaiDiv.hidden = false;
+
+    setTimeout(() => {
+      itaiDiv.hidden = true;
+    }, 300);
+  }
+
+  // 最低クリック回数をチェックする
+  if(checkIsDone()){
+    // 完了を表示
+    const doneDiv = document.getElementById("done");
+    doneDiv.hidden = false;
+  }
+}
+
 function onClickTsubo1 () {
-
-  accumulateClick.tsubo1 += 1;
-
-  console.log("tsubo1 clicked", accumulateClick.tsubo1)
-
-  // 痛っと表示する。
-  const itaiDiv = document.getElementById("itai");
-  itaiDiv.hidden = false;
-
-  setTimeout(() => {
-    itaiDiv.hidden = true;
-  }, 3000);
-
-  // 最低クリック回数をチェックする
-  if(checkIsDone()){
-    // 完了を表示
-    const doneDiv = document.getElementById("done");
-    doneDiv.hidden = false;
-  }
+  onClickTsubo('tsubo1');
 }
+
 function onClickTsubo2 () {
-
-  accumulateClick.tsubo2 += 1;
-
-  console.log("tsubo2 clicked", accumulateClick.tsubo2)
-
-  // 痛っと表示する。
-  const itaiDiv = document.getElementById("itai");
-  itaiDiv.hidden = false;
-
-  setTimeout(() => {
-    itaiDiv.hidden = true;
-  }, 3000);
-
-  // 最低クリック回数をチェックする
-  if(checkIsDone()){
-    // 完了を表示
-    const doneDiv = document.getElementById("done");
-    doneDiv.hidden = false;
-  }
+  onClickTsubo('tsubo2');
 }
+
 function onClickTsubo3 () {
-
-  accumulateClick.tsubo3 += 1;
-
-  console.log("tsubo3 clicked", accumulateClick.tsubo3)
-
-  // 痛っと表示する。
-  const itaiDiv = document.getElementById("itai");
-  itaiDiv.hidden = false;
-
-  setTimeout(() => {
-    itaiDiv.hidden = true;
-  }, 3000);
-
-  // 最低クリック回数をチェックする
-  if(checkIsDone()){
-    // 完了を表示
-    const doneDiv = document.getElementById("done");
-    doneDiv.hidden = false;
-  }
+  onClickTsubo('tsubo3');
 }
+
 function onClickTsubo4 () {
-
-  accumulateClick.tsubo4 += 1;
-
-  console.log("tsubo4 clicked", accumulateClick.tsubo4)
-
-  // 痛っと表示する。
-  const itaiDiv = document.getElementById("itai");
-  itaiDiv.hidden = false;
-
-  setTimeout(() => {
-    itaiDiv.hidden = true;
-  }, 3000);
-
-  // 最低クリック回数をチェックする
-  if(checkIsDone()){
-    // 完了を表示
-    const doneDiv = document.getElementById("done");
-    doneDiv.hidden = false;
-  }
+  onClickTsubo('tsubo4');
 }
+
 function onClickTsubo5 () {
-
-  accumulateClick.tsubo5 += 1;
-
-  console.log("tsubo5 clicked", accumulateClick.tsubo5)
-
-  // 痛っと表示する。
-  const itaiDiv = document.getElementById("itai");
-  itaiDiv.hidden = false;
-
-  setTimeout(() => {
-    itaiDiv.hidden = true;
-  }, 3000);
-
-  // 最低クリック回数をチェックする
-  if(checkIsDone()){
-    // 完了を表示
-    const doneDiv = document.getElementById("done");
-    doneDiv.hidden = false;
-  }
+  onClickTsubo('tsubo5');
 }
-
 
 
 /********************* */
